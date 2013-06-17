5d25fd911f847392e4867bc870f61c34c57177a6
shaun gilchrist <shaun.gilchrist@venafi.com>
2013-06-16 23:44:01 -0600

    adding install and run-valid-tests scripts as well as fixing a whitespace test and adding default tag reader

--- platforms/clojure/runner/src/runner/core.clj
+++ platforms/clojure/runner/src/runner/core.clj
@@ -1,15 +1,14 @@
 (ns runner.core
   (:require [clojure.edn :as edn]
             [clojure.string :as string])
+  (:use clojure.pprint)
   (:import java.io.File))
 
 (defn tagged-item [tag value] {:tag tag :value value})
 
 (defn comp-forms [clj-file edn-file]
-  (do (println "LOAD" edn-file)
   (= (read-string (slurp clj-file))
-     (edn/read-string (slurp edn-file)))))
-
+     (edn/read-string {:eof nil :default tagged-item} (slurp edn-file))))
 
 (defn get-current-directory []
   (. (File. ".") getCanonicalPath))
@@ -19,8 +18,10 @@
         edn-dir (str cur-dir "/../../../valid-edn/")
         clj-dir (str cur-dir "/../")
         edn-files (.listFiles (File. edn-dir))]
-       (doseq [file edn-files]
-              (let [file-name (first (string/split (. file getName) #"\."))
-                    result (comp-forms (str clj-dir file-name ".clj")
-                                       (str edn-dir file-name ".edn"))]
-                    (println "TEST:" file-name (pr-str result))))))
+       (pprint 
+         (into {} 
+               (map (fn [file]
+                        (let [file-name (first (string/split (. file getName) #"\."))
+                              result (comp-forms (str clj-dir file-name ".clj")
+                                                 (str edn-dir file-name ".edn"))]
+                             [(symbol file-name) result])) edn-files)))))
